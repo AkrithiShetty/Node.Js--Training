30d7c54162fee56736f94720ab09aab8d0500da0
AkrithiShetty <shettakrithi@gmail.com>
2020-07-27 14:36:28 +0530

    Commit Application

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 	useNewUrlParser: true,
@@ -7,46 +8,92 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const Tasks = mongoose.model('Tasks', {
 	description: {
-		type: String
+		type: String,
+		required: true,
+		trim: true
 	},
 	completed: {
-		type: Boolean
+		type: Boolean,
+		default: false
 	}
+});
+
+const task = new Tasks({
+	description: '    Cleaning'
+	//completed: true
 });
 
 // const task = new Tasks({
-// 	description: 'Cleaning',
-// 	completed: true,
+// 	completed: true
 // });
 
-// task
-// 	.save()
-// 	.then(() => {
-// 		console.log(task);
-// 	})
-// 	.catch((error) => {
-// 		console.log(error);
-// 	});
-
-const User = mongoose.model('User', {
-	name: {
-		type: String,
-		required: true
-	},
-	age: {
-		type: Number
-	}
-});
-
-const me = new User({
-	name: 'Anvitha'
-	// age: 22,
-});
-
-me.save()
+task
+	.save()
 	.then(() => {
-		console.log(me);
+		console.log(task);
 	})
 	.catch((error) => {
 		console.log(error);
 	});
+
+const User = mongoose.model('User', {
+	name: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+		lowercase: true,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				throw new Error('Invalid Email Id');
+			}
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		trim: true,
+		validate(value) {
+			if (value.length <= 6) {
+				throw new Error('Passsword must be greater than 6 characters');
+			} else if (value == 'password') {
+				throw new Error('Change the password');
+			}
+		}
+	},
+	age: {
+		type: Number,
+		default: 0,
+		validate(value) {
+			if (value < 0) {
+				throw new Error('Age must be a positive number');
+			}
+		}
+	}
+});
+
+// const me = new User({
+// 	name: '  Akrithi',
+// 	email: 'Akrithishetty14@gmail.com',
+// 	password: 'juego@1234',
+// 	age: 22
+// });
+
+// const me = new User({
+// 	name: '  Suraksha',
+// 	email: 'SuRakshashetty29@gmail.com',
+// 	password: '  hi',
+// 	age: 21
+// });
+
+// me.save()
+// 	.then(() => {
+// 		console.log(me);
+// 	})
+// 	.catch((error) => {
+// 		console.log(error);
+// 	});
