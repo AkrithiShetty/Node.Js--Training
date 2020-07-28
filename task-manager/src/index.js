70faaa656184f9549c68dc3ed605aa3a30d219f0
AkrithiShetty <shettakrithi@gmail.com>
2020-07-28 20:41:37 +0530

    Updated Changes

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,83 +1,29 @@
 const express = require('express');
 require('./db/mongoose');
-const User = require('./models/user');
-const Tasks = require('./models/task');
+const userRouter = require('./routers/user.js');
+const taskRouter = require('./routers/task.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.post('/users', async (req, res) => {
-	const user = new User(req.body);
-
-	try {
-		await user.save();
-		res.status(201).send(user);
-	} catch (e) {
-		res.status(400).send(e);
-	}
-	// user.save().then(() => {
-	// 		res.status(201).send(user);
-	// 	}).catch((error) => {
-	// 		res.status(400).send(error);
-	// 	});
-});
-
-app.get('/users', async (req, res) => {
-	try {
-		const users = await User.find({});
-		res.send(users);
-	} catch (e) {
-		res.status(500).send(e);
-	}
-});
-
-app.get('/users/:id', async (req, res) => {
-	const _id = req.params.id;
-	try {
-		const user = await User.findById(_id);
-		if (!user) {
-			return res.status(404).send();
-		}
-		res.send(user);
-	} catch (e) {
-		res.status(500).send(e);
-	}
-});
-
-app.post('/tasks', async (req, res) => {
-	const task = new Tasks(req.body);
-	try {
-		await task.save();
-		res.status(201).send(task);
-	} catch (error) {
-		res.status(500).send(error);
-	}
-});
-
-app.get('/tasks', async (req, res) => {
-	try {
-		const task = await Tasks.find({});
-		res.status(200).send(task);
-	} catch (error) {
-		res.status(400).send(error);
-	}
-});
-
-app.get('/tasks/:id', async (req, res) => {
-	const _id = req.params.id;
-	try {
-		const task = await Tasks.findById(_id);
-		if (!task) {
-			return res.status(404).send(tasks);
-		}
-		res.send(task);
-	} catch (error) {
-		res.status(500).send(error);
-	}
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
 	console.log('Server is up on port ', port);
 });
+
+// const bcrypt = require('bcryptjs');
+// const myFunction = async () => {
+// 	const password = 'red1234';
+// 	const hashedPassword = await bcrypt.hash(password, 8);
+
+// 	console.log(password, hashedPassword);
+
+// 	const isMatch = await bcrypt.compare('Red1234', hashedPassword);
+
+// 	console.log(isMatch);
+// };
+
+// myFunction();
